970b5147d542f071e3e61ff889fc384c1ad53d96
Weston <barnesweston@gmail.com>
2022-04-06 15:31:05 -0500

    update license list

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,29 +6,64 @@ function renderLicenseBadge(license) {
   if (!license) {
     return '';
   }
+
   switch (license) {
     case 'Apache':
       return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
     case 'MIT':
       return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'ISC':
-      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
-    case 'IBM':
-      return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+    case 'BSD 3':
+      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    case 'BSD 2':
+      return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
     case 'Eclipse':
       return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
-    case 'Boost':
-      return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
   };
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  switch (license) {
+    case 'Apache':
+
+    case 'MIT':
+
+    case 'BSD 3':
+
+    case 'BSD 2':
+
+    case 'Eclipse':
+
+    case 'GNU':
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  switch (license) {
+    case 'Apache':
+
+    case 'MIT':
+
+    case 'BSD 3':
+
+    case 'BSD 2':
+
+    case 'Eclipse':
+
+    case 'GNU':
+  }    
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
