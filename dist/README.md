eaa3f0991818da8937a29056043f9e31ae60715c
Weston <barnesweston@gmail.com>
2022-04-07 20:53:13 -0500

    finish license section of markdown generation

--- dist/README.md
+++ dist/README.md
@@ -1,9 +1,9 @@
-# readme  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# readme gen  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
   ## Description:  
-  - generate a readme
+  - generate a readme file
 
   ## Installation   
-  - npm install
+  - 
 
   ## Usage   
   - 
@@ -15,9 +15,9 @@
   - 
 
   ## Licences
-  
+  [MIT License](https://choosealicense.com/licenses/mit/)
 
   ## Questions
   Here is a link to my [Github page](https://github.com/bkwes)  
   If you would like to contact me for further information or details on collaboration  
-  Email weston: barnesweston
+  Email Wes: barnesweston
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,16 +8,10 @@ function renderLicenseBadge(license) {
   }
 
   switch (license) {
-    case 'Apache':
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
     case 'MIT':
       return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'BSD 3':
-      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
     case 'BSD 2':
       return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
-    case 'Eclipse':
-      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
     case 'GNU':
       return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
   };
@@ -30,40 +24,14 @@ function renderLicenseLink(license) {
     return '';
   }
   switch (license) {
-    case 'Apache':
-
     case 'MIT':
-
-    case 'BSD 3':
-
+      return '[MIT License](https://choosealicense.com/licenses/mit/)';
     case 'BSD 2':
-
-    case 'Eclipse':
-
+      return '[BSD 2-Clause License](https://choosealicense.com/licenses/bsd-2-clause/)';
     case 'GNU':
+      return '[GNU General Public License](https://choosealicense.com/licenses/gpl-2.0/)';
   }
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return '';
-  }
-  switch (license) {
-    case 'Apache':
-
-    case 'MIT':
-
-    case 'BSD 3':
-
-    case 'BSD 2':
-
-    case 'Eclipse':
-
-    case 'GNU':
-  }    
-}
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -84,7 +52,7 @@ function generateMarkdown(data) {
   - ${data.tests}
 
   ## Licences
-  
+  This application is covered under the ${renderLicenseLink(data.license)}.
 
   ## Questions
   Here is a link to my [Github page](https://github.com/${data.github})  
